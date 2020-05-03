6e7f5c2629991a4db973ae4d3bbff446929ccac2
jingshenglyu <jingshenglyu@gmail.com>
2020-05-03 22:47:42 +0200

    example 6.21 a shallow copy

---
+++ C++_ZL/chapter6_ArrayAndPointer/Pointer/6_21_ShallowCopy.cpp
@@ -0,0 +1,82 @@
+// 6_21 A Shallow Copy
+#include<iostream>
+#include<cassert>
+using namespace std;
+
+class Point
+{
+private:
+    int x, y;
+public:
+    Point():x(0), y(0)
+    {
+        cout << "Default Constructor called." << endl;
+    }
+    Point(int x, int y):x(x),y(y)
+    {
+        cout << "Constructor called." << endl;
+    }
+    ~Point(){ cout << "Destructor called." << endl;}
+
+    int getX() const {return x;}
+    int getY() const {return y;}
+    void move (int newX, int newY)
+    {
+        x = newX;
+        y = newY;
+    }
+};
+
+class ArrayOfPoints //class of dynamic array 
+{
+private:
+    Point *points; // point to the first address of dynamic array
+    int size;   // size of array
+public:
+    ArrayOfPoints(int size):size(size)
+    {
+        points = new Point[size];
+    }
+    ~ArrayOfPoints()
+    {
+        cout << "Deleting ... " << endl;
+        delete[] points;
+    }
+    Point& element(int index)
+    {
+        assert(index >= 0 && index < size);
+        return points[index];
+    }
+};
+
+int main()
+{
+    int count;
+    cout << "Please enter the count of points: ";
+    cin >> count;
+    ArrayOfPoints pointsArray1(count);  //create a new array obj.
+    pointsArray1.element(0).move(5,10);
+    pointsArray1.element(1).move(15,20);
+
+    ArrayOfPoints pointsArray2=pointsArray1;    //create a copy array obj.
+    cout << "Copy of pointsArray1:" << endl;
+
+    cout << "Point_0 of array2: " << pointsArray2.element(0).getX() << ","
+         << pointsArray2.element(0).getY() << endl;
+    
+    cout << "Point_1 of array2: " << pointsArray2.element(1).getX() << ","
+         << pointsArray2.element(1).getY() << endl;
+
+    pointsArray1.element(0).move(25,30);
+    pointsArray1.element(1).move(35,40);
+    cout << "After the moving of pointsArray1:" << endl;
+
+    cout << "Point_0 of array2: " << pointsArray2.element(0).getX() << ","
+         << pointsArray2.element(0).getY() << endl;
+    
+    cout << "Point_1 of array2: " << pointsArray2.element(1).getX() << ","
+         << pointsArray2.element(1).getY() << endl;
+
+    return 0;
+}
+
